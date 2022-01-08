d458eb513c94b506efbf909abda85c2520b5a21d
chiranjitmishra5 <chiranjitmishra5@gmail.com>
2022-01-08 23:36:00 +0530

    first commit by master branch

---
+++ calculator.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class calculator{
+	float a,b,res;
+	char choice,ch;
+	Scanner scan=new Scanner(System.in);
+		do{
+			System.out.println("1 add Two number ");
+			System.out.println("2.exit");
+
+
